9c6adc4b3467c46bc4321c32bb8c20898a20b403
arnold <arnoldcai@berkeley.edu>
2023-10-28 15:40:19 -0700

    added pygame game loop

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ running = True
 
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # pygame.QUIT event means the user python3 -m pip install pygame
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
